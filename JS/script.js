26c5bda583a42a0c37934b9975e8bfeee9bf8db1
Ahasan Ara Shimi <ahasanarashimi@gmail.com>
2025-03-01 04:29:02 +0600

    Initialization

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+document.getElementById("theme-btn").addEventListener("click", function() {
+  const colors = ['bg-[#DED7FB]' , 'bg-[#F7E3EE]' , 'bg-[#FBFAF5]' , 'bg-[#FBF4E2]' , 'bg-[#D8F0FA]' , 'bg-[#C7DEFA]' , 'bg-[#E0E5B6]' ,'bg-[#F2E2B1]' ,'bg-[#E8F9FF]' ,'bg-[#FFF2C2]' ,'bg-[#D9DFC6]' ,'bg-[#EABDE6]' ,'bg-[#E8F9FF]' ,'bg-[#FDF7F4]' ,'bg-[#E7CCCC]' ,'bg-[##EDE8DC]' ,'bg-[##F6EACB]' ,'bg-[#CAD0DD]' ,'bg-[#EDF1E9]'] 
+  const bodyColor = document.body; 
+ 
+  const randomColor = colors[Math.floor(Math.random() * colors.length)];
+
+  bodyColor.className = ''; 
+  bodyColor.classList.add(randomColor);
+});
+
+
+
